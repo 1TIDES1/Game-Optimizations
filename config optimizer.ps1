270fc80d9d99419ef01dc45aec7f477458c2f68a
1TIDES1 <80750812+1TIDES1@users.noreply.github.com>
2022-09-23 12:23:52 +0100

    Add files via upload

---
+++ config optimizer.ps1
@@ -0,0 +1,27 @@
+$arg1=$args[0]
+$arg2=$args[1]
+$arg3=$args[2]
+$arg4=$args[3]
+$arg5=$args[4]
+
+set-location $env:TEMP
+cd 'Apex_Configs'
+$content = Get-Content -path 'videoconfig.txt'
+$newContent = $content -replace '1920', $arg1
+$newContent | Set-Content -Path 'videoconfig.txt'
+
+$content = Get-Content -path 'videoconfig.txt'
+$newContent = $content -replace '12', $arg2
+$newContent | Set-Content -Path 'videoconfig.txt'
+
+$content = Get-Content -path 'videoconfig.txt'
+$newContent = $content -replace 'csm_res', $arg4
+$newContent | Set-Content -Path 'videoconfig.txt'
+
+$content = Get-Content -path 'videoconfig.txt'
+$newContent = $content -replace 'csm', $arg3
+$newContent | Set-Content -Path 'videoconfig.txt'
+
+$content = Get-Content -path 'videoconfig.txt'
+$newContent = $content -replace '0000000', $arg5
+$newContent | Set-Content -Path 'videoconfig.txt'
