8b6657193b36c70d8fe9974f61c74126b3d2fbf5
1TIDES1 <80750812+1TIDES1@users.noreply.github.com>
2022-09-24 08:50:50 +0100

    Update config optimizer.ps1

--- config optimizer.ps1
+++ config optimizer.ps1
@@ -4,6 +4,7 @@ $arg3=$args[2]
 $arg4=$args[3]
 $arg5=$args[4]
 $arg6=$args[5]
+$arg6=$args[6]
 
 set-location $env:TEMP
 cd 'Apex_Configs'
@@ -27,13 +28,22 @@ $content = Get-Content -path 'videoconfig.txt'
 $newContent = $content -replace '0000000', $arg5
 $newContent | Set-Content -Path 'videoconfig.txt'
 
-set-location $env:USERPROFILE
-cd 'Documents'
-$fps_line = Get-Content 'Benchmark.txt' | Select -Index 4
-$fps_count = $fps_line -replace "[^0-9]" , ''
-$fps_total = $fps_count / 10
-$fps_round = [math]::Round($fps_total)
-$fps_accurate = $fps_round - 1000
+Function Accurate_Frame_Cap
+{
+ set-location $env:USERPROFILE
+ cd 'Documents'
+ $fps_line = Get-Content 'Benchmark.txt' | Select -Index 4
+ $fps_count = $fps_line -replace "[^0-9]" , ''
+ $fps_total = $fps_count / 10
+ $fps_round = [math]::Round($fps_total)
+ Set-Variable -scope 1 -Name "fps_accurate" -Value '$fps_round - 1000'
+}
+
+if $arg6=yes (
+  Accurate_Frame_Cap
+) ELSE if (
+  $fps_accurate=0
+)
 
 set-location $env:TEMP
 cd 'Apex_Configs'
