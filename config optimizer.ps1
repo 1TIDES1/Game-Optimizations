16486a26cfe94e3ae5b2b4ce3f5c3de48ef997ea
1TIDES1 <80750812+1TIDES1@users.noreply.github.com>
2022-09-26 11:36:33 +0100

    Update config optimizer.ps1

--- config optimizer.ps1
+++ config optimizer.ps1
@@ -30,13 +30,14 @@ Function Accurate_Frame_Cap
 #---------[FRAME CAP FUNCTION]---------#
 
 Function Super_Glide
-
+{
  if ($superglideChoice -eq 'no')
  {
   $content = Get-Content -path 'autoexec.cfg'
   $newContent = $content -replace 'exec superglide1.cfg                                                    // Activate superglide script (Read more about it in superglide.cfg, superglide1.cfg and superglide2.cfg) (Works)', ""
   $newContent | Set-Content -Path 'autoexec.cfg'
  }
+}
 
 #---------[CONFIG EDITOR FUNCTION]---------#
 
