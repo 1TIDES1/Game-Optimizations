275d613935e0294792dbdc64de0ac2b901ca6298
1TIDES1 <80750812+1TIDES1@users.noreply.github.com>
2022-09-26 11:43:11 +0100

    Update config optimizer.ps1

--- config optimizer.ps1
+++ config optimizer.ps1
@@ -36,6 +36,7 @@ Function Super_Glide
   Set-Variable -scope 1 -Name "SG_Choice" -Value ''
  else {
   $SG_Choice = 'exec superglide1.cfg                                                    // Activate superglide script (Read more about it in superglide.cfg, superglide1.cfg and superglide2.cfg) (Works)'
+  }
  }
 }
 
